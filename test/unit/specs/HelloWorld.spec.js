f6d20ea4f89eaf8fff6ccd0129c8c63ac6e1cf45
Lucas Cesar <lucas.cesar@prosas.com.br>
2020-07-27 18:03:54 -0300

    Attemp to fiz HelloWorld Test

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -5,7 +5,13 @@ describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.display-2 h1').textContent)
-      .toEqual('Welcome to Vuetify');
+    console.log("--- T E S T ---");
+    console.log(vm)
+    console.log(vm.$el)
+    console.log("Text content")
+    console.log(vm.$el.querySelector('.hello').textContent)
+    console.log("End of Text content")
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .toEqual('Welcome to Your Vue.js App');
   });
 });
